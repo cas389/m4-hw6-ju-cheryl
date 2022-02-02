78348dcb98a22f38b08007fcb2ef6056e06917e0
Cheryl Ju <cheryl.as.ju@gmail.com>
2022-02-02 16:00:36 -0500

    Worked on first two sections.

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function getPizzaOrder() {
   var pizza = {
     // set the 'cost' property to the basePrice
     // YOUR CODE HERE
+    cost: basePrice
   }
 
-  var crust = prompt(
-    `Please enter the type of crust (${formatPrice(thickCrustUpcharge)} upcharge for Thick crust)`
+  var crust = prompt(`Please enter the type of crust (${formatPrice(thickCrustUpcharge)} upcharge for Thick crust)`
   )
   // set the pizza object's 'crust' property to the user's response
   // HINT: prompt() returns a string
@@ -22,6 +22,15 @@ function getPizzaOrder() {
   // if the user specified 'thick' crust, add thickCrustUpcharge
   // to pizza.cost
   // YOUR CODE HERE
+  crust = crust.trim().toLowerCase();
+  console.log("You ordered", crust, "crust.");
+
+  if (crust === "thick") {
+    pizza.cost = basePrice + thickCrustUpcharge;
+  } else {
+    pizza.cost = basePrice;
+  }
+
 
   var toppings = prompt("Please enter additional toppings (comma separated)")
   // HINT: prompt() will return an empty string "" if the user presses 'OK' without entering a value
